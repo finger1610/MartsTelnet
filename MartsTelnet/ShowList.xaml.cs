c1610d33edc42dfe6825d00ae0e0e5dae07f3418
FiNGeR <martsev.96@gmail.com>
2021-12-08 17:36:54 +0700

    Добалвена запись логов в файл

--- MartsTelnet/ShowList.xaml.cs
+++ MartsTelnet/ShowList.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -40,5 +41,46 @@ namespace MartsTelnet
         {
             this.Close();
         }
+
+        private void btnSaveToFile_Click(object sender, RoutedEventArgs e)
+        {
+            var fileDialog = new System.Windows.Forms.SaveFileDialog();
+            fileDialog.FileName = "file";
+            fileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Лог файлы(*.log)|*.log|Все файлы (*.*)|*.*" ;
+            var result = fileDialog.ShowDialog();
+            switch (result)
+            {
+                case System.Windows.Forms.DialogResult.OK:
+                    var file = fileDialog.FileName;
+  
+                    //Считывание из файла
+                    try
+                    {
+                        using (FileStream fs = new FileStream(file, FileMode.OpenOrCreate))
+                        {
+                            using (StreamWriter sw = new StreamWriter(fs, System.Text.Encoding.UTF8))
+                            {
+                               foreach (string iter in _IP)
+                               {
+                                    sw.WriteLine(iter);
+                                    sw.WriteLine("##############################");
+                                }
+                            }
+                        }
+                        MessageBox.Show("Содержимое сохранено в файле " + file);
+                        
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Возникла ошибка при записи:\n" + ex.Message.ToString());
+                    }
+                    break;
+     
+                default:
+
+                    break;
+            }
+        }
     }
+    
 }
